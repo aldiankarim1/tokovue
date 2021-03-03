9c2c245ebd993c1c183ec42f44d3bbd9025d0b9c
Aldian Karim <turafn@gmail.com>
2021-03-03 18:32:22 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+   <HeaderAldian />
+   <HeroAldian />
+   <BennerAldian />
+   <InstaAldian />
+   <PatnerAldian />
+   <FooterAldian />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderAldian from "@/components/HeaderAldian.vue";
+import HeroAldian from "@/components/HeroAldian.vue";
+import BennerAldian from "@/components/BennerAldian.vue";
+import InstaAldian from "@/components/InstaAldian.vue";
+import PatnerAldian from "@/components/PatnerAldian.vue";
+import FooterAldian from "@/components/FooterAldian.vue";
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderAldian,
+    HeroAldian,
+    BennerAldian,
+    InstaAldian,
+    PatnerAldian,
+    FooterAldian
+  }
+};
+</script>
